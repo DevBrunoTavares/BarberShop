38d258e2e1d1dc644acc83c8134daabfd4207308
BrunoTavaresQA <115037702+BrunoTavaresQA@users.noreply.github.com>
2024-05-22 16:48:33 -0300

    Start Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.barbershop"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.barbershop"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    viewBinding{
+        enable = true
     }
 }
 
--- app/src/main/java/com/example/barbershop/MainActivity.kt
+++ app/src/main/java/com/example/barbershop/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.barbershop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.barbershop.databinding.ActivityMainBinding
+import com.example.barbershop.view.Home
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val edtNome = binding.edtNome
+
+        binding.btnAgendar.setOnClickListener {
+            val agendar = binding.edtNome.text.toString()
+
+
+            if (agendar == "") {
+                // mostrar  mensagem para o usuario
+
+                Snackbar.make(
+                    edtNome, "Preencha seu nome",
+                    Snackbar.LENGTH_LONG
+                )
+                    .show()
+            } else {
+                navegarPraHome(agendar)
+            }
+        }
+    }
+
+    private fun navegarPraHome(agendar: String) {
+        val intent = Intent(this, Home::class.java)
+        intent.putExtra("Agendar", agendar)
+        startActivity(intent)
     }
 }
